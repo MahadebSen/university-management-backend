4a16c5ca2dcb839e51870f1cf2a7be5f0159f40e
Mahadeb Sen <mahadebsen237@gmail.com>
2023-06-02 00:37:20 +0530

    [DONE]esline abd prettier added

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application } from "express";
-import cors from "cors";
+import express, { Application } from 'express'
+import cors from 'cors'
 // application routes
 // import userRoute from "./app/modules/user/user.route";
 
-const app: Application = express();
+const app: Application = express()
 
 // using cors
-app.use(cors());
+app.use(cors())
 
 // parseing data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // app.use("/api/v1/user", userRoute);
 
-export default app;
+export default app
